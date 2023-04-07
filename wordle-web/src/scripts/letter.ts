3bbc2d6a889cfee2dc73ea73a60298572ecf8393
daisypalencia <dpalencia@ewu.edu>
2023-04-07 01:07:02 -0700

    tests

--- wordle-web/src/scripts/letter.ts
+++ wordle-web/src/scripts/letter.ts
@@ -1,6 +1,6 @@
 // Class that represents a letter in the wordle word and status
 // of the letter in the wordle word 
-// comment
+// comments
 export enum LetterStatus {
   NotGuessed = 0,
   Correct,
