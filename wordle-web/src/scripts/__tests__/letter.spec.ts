a45c2fe5be54b0f3450af4253d31505005dfff57
daisypalencia <dpalencia@ewu.edu>
2023-04-23 17:19:12 -0700

    tests changes

--- wordle-web/src/scripts/__tests__/letter.spec.ts
+++ wordle-web/src/scripts/__tests__/letter.spec.ts
@@ -1,16 +1,16 @@
 import { describe, it, expect } from 'vitest'
 import { Letter, LetterStatus } from '@/scripts/letter'
-//parce la en la vida es una
+
 describe('Letter', () => {
   it('Gets Correct Color', () => {
     const letter = new Letter('a')
     letter.status = LetterStatus.Correct
-    expect(letter.color).toBe('green')
+    expect(letter.color).toBe('correct')
   })
 
   it('Gets Misplaced Color', () => {
     const letter = new Letter('a')
     letter.status = LetterStatus.Misplaced
-    expect(letter.color).toBe('orange')
+    expect(letter.color).toBe('misplaced')
   })
 })
